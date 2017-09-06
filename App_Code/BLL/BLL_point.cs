b96362b86fe8eb515c4d96318d0ae5eaacbf545b
lijiudan <lizhijan@163.com>
2017-09-06 16:04:05 +0800

    ok

--- App_Code/BLL/BLL_point.cs
+++ App_Code/BLL/BLL_point.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data;
+
+using Newtonsoft.Json;
 using DAO;
 
 namespace BLL
@@ -19,10 +21,286 @@ namespace BLL
             //
         }
 
+        /// <summary>
+        /// 热点html代码
+        /// </summary>
+        private string htmlcodemodel = "<img src=\"../image/temp/$title_img_url$\" style=\"width:100%;\" alt=\"$title$\"/> <div>$description$</div>";
+
+
+        public string Loadindustry()
+        {
+            DataTable dt = new DAO_project().GetList();
+            string json = JsonConvert.SerializeObject(dt);
+            return json;
+        }
+        
         public DataTable getPointByIVID(string id)
         {
             DataTable dt = new DAO_point().GetByIVID(id);
             return dt;
+        }
+
+
+        public string delPointByID(int id)
+        {          
+
+           bool isok= new DAO_point().DeleteByID(id);
+
+           if (isok == true)
+           {
+               var send = new
+               {
+                   msg = "操作成功！",
+                   success = "true"
+               };
+               return JsonConvert.SerializeObject(send);
+           }
+           else
+           {
+               var send = new
+               {
+                   msg = "操作失败！",
+                   success = "false"
+               };
+               return JsonConvert.SerializeObject(send);
+           }
+   
+        
+        }
+
+        /// <summary>
+        /// 返回热点信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string getPointByID(int id)
+        {
+           
+            DataTable dt = new DAO_point().GetByID(id);
+            if (dt.Rows.Count > 0)
+            {
+                var send = new
+                {
+                    id = dt.Rows[0]["id"].ToString(),
+                    iv_id = dt.Rows[0]["iv_id"]!=null? dt.Rows[0]["iv_id"].ToString():"",
+                    title = dt.Rows[0]["title"] != null ? dt.Rows[0]["title"].ToString() : "",
+                    title_en = dt.Rows[0]["title_en"] != null ? dt.Rows[0]["title_en"].ToString() : "",
+                    title_img_url = dt.Rows[0]["title_img_url"] != null ? dt.Rows[0]["title_img_url"].ToString() : "",
+                    description = dt.Rows[0]["description"] != null ? dt.Rows[0]["description"].ToString() : "",
+                    description_mod = dt.Rows[0]["description_mod"] != null ? dt.Rows[0]["description_mod"].ToString() : "",
+                    description_en = dt.Rows[0]["description_en"] != null ? dt.Rows[0]["description_en"].ToString() : "",
+                    description_en_mod = dt.Rows[0]["description_en_mod"] != null ? dt.Rows[0]["description_en_mod"].ToString() : "",
+                    locked = dt.Rows[0]["locked"] != null ? dt.Rows[0]["locked"].ToString() : ""
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            
+        }
+        /// <summary>
+        /// 返回json列表
+        /// </summary>
+        /// <param name="pageNum"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="title"></param>
+        /// <param name="description"></param>
+        /// <param name="orderBy"></param>
+        /// <returns></returns>
+        public string getList(int pageNum, int pageSize, string title, string description)
+        {
+            string strjson = "";
+            string orderBy = " id asc";
+            int total = 0;
+            DataTable dt = new DAO_point().GetList(pageNum, pageSize, title, description, orderBy, out total);
+
+            strjson=ToJson(dt, total);
+            return strjson;
+        }
+
+
+        /// <summary>
+        /// 添加点信息
+        /// </summary>
+        /// <param name="iv_id"></param>
+        /// <param name="title"></param>
+        /// <param name="title_en"></param>
+        /// <param name="title_img_url"></param>
+        /// <param name="description"></param>
+        /// <param name="description_en"></param>
+        /// <param name="locked"></param>
+        /// <returns></returns>
+        public string addPoint(string iv_id, string title, string title_en, string title_img_url, string description,  string description_en,  bool locked)
+        {    
+
+            string description_mod = create(title, title_img_url, description);
+            string description_en_mod = create(title_en, title_img_url, description_en);
+            bool isok = new DAO_point().Add(iv_id, title, title_en, title_img_url, description, description_mod, description_en, description_en_mod, locked);
+
+            if (isok == true)
+            {
+                var send = new
+                {
+                    msg = "操作成功！",
+                    success = "true"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+
+        }
+
+        /// <summary>
+        /// 修改点信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="iv_id"></param>
+        /// <param name="title"></param>
+        /// <param name="title_en"></param>
+        /// <param name="title_img_url"></param>
+        /// <param name="description"></param>
+        /// <param name="description_en"></param>
+        /// <param name="locked"></param>
+        /// <returns></returns>
+        public string edit(int id, string iv_id, string title, string title_en, string title_img_url, string description, string description_mod, string description_en, string description_en_mod, bool locked)
+        {
+          
+
+            //20170904 李久丹 不锁定则修改模板
+            if (locked == false)
+            {
+                 description_mod = create(title, title_img_url, description);
+                 description_en_mod = create(title_en, title_img_url, description_en);
+
+            }
+            bool isok = new DAO_point().Update(id,  iv_id,  title,  title_en,  title_img_url,  description,  description_mod,  description_en,  description_en_mod,  locked);
+
+            if (isok == true)
+            {
+                var send = new
+                {
+                    msg = "操作成功！",
+                    success = "true"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+           
+
+        }
+
+        public string recreate(string strid)
+        {
+          
+
+            bool locked = true;
+            if (strid == "0")
+            { //重新生成未锁定的
+                locked = true;
+            }
+            else
+            {//重新生成所有
+                locked = false;
+            }
+            
+            bool isok = new DAO_point().recreate(locked, htmlcodemodel);
+
+            if (isok == true)
+            {
+                var send = new
+                {
+                    msg = "操作成功！",
+                    success = "true"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+          
+        
+        }
+       /// <summary>
+       /// 返回根据模板显示页面生成的html代码
+       /// </summary>
+       /// <param name="title"></param>
+       /// <param name="title_img_url"></param>
+       /// <param name="description"></param>
+       /// <returns></returns>
+        private string create(string title, string title_img_url, string description)
+        {
+            string strjson = "";
+            strjson = htmlcodemodel;
+            strjson = strjson.Replace("$title$",title);
+            strjson = strjson.Replace("$title_img_url$", title_img_url);
+            strjson = strjson.Replace("$description$", description);
+            return strjson;
+        }
+
+
+        /// <summary>
+        /// 返回根据模板显示页面生成的html文件
+        /// </summary>
+        /// <param name="cthtml"></param>
+        /// <returns></returns>
+        private bool cthtml(string cthtml)
+        {
+            bool isok = true;
+
+
+            return isok;
+        
+        }
+        private string ToJson(DataTable table, int total)
+        {
+            DataGridBean dgb = new DataGridBean();
+            dgb.Total = total.ToString();
+            dgb.Rows = table;
+            string json = JsonConvert.SerializeObject(dgb);
+            return json;
+        }
+
+        /// <summary>
+        ///DataGridBean 的摘要说明
+        /// </summary>
+        private class DataGridBean
+        {
+            public DataGridBean() { }
+
+            [JsonProperty("total")]
+            public string Total { get; set; }
+
+            [JsonProperty("rows")]
+            public DataTable Rows { get; set; }
         }
     } 
 }
--- App_Code/BLL/BLL_project.cs
+++ App_Code/BLL/BLL_project.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using Newtonsoft.Json;
 using DAO;
 
 namespace BLL
@@ -23,7 +24,7 @@ namespace BLL
         public DataTable getHotProject(int number)
         {
             int count = 0;
-            DataTable dt = new DAO_project().GetList(1, number, "", true, 0, 0, "hotorder asc", out count);
+            DataTable dt = new DAO_project().GetList(1, number, "", "", true, 0, 0, "hotorder asc", out count);
             return dt;
         }
 
@@ -32,8 +33,102 @@ namespace BLL
             int s = 0;
             int.TryParse(sort, out s);
             int count = 0;
-            DataTable dt = new DAO_project().GetList(1, pageSize, param, false, 0, s, "name", out count);
+            DataTable dt = new DAO_project().GetList(1, pageSize, param, param, false, 0, s, "name", out count);
             return dt;
+        }
+
+        public string getProjectByIVID(string IVID)
+        {            
+            DataTable dt = new DAO_project().GetByIVID(IVID);
+            if (dt.Rows.Count > 0)
+            {
+                var send = new
+                {
+                    id = dt.Rows[0]["id"].ToString(),
+                    iv_id = dt.Rows[0]["iv_id"] != null ? dt.Rows[0]["iv_id"].ToString() : "",
+                    title = dt.Rows[0]["name"] != null ? dt.Rows[0]["name"].ToString() : "",
+                    title_en = dt.Rows[0]["name_en"] != null ? dt.Rows[0]["name_en"].ToString() : "",
+                    ishot = dt.Rows[0]["ishot"] != null ? dt.Rows[0]["ishot"].ToString() : "",
+                    industry_id = dt.Rows[0]["industry_id"] != null ? dt.Rows[0]["industry_id"].ToString() : ""
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+        
+        }
+
+        public string Add(string name_en,string name, string description,   string iv_id,  bool ishot,  int industry_id)
+        {
+
+            string img_url="image/project/"+iv_id+"/title.jpg";
+            string poi_id="0";
+            int listorder=0;
+            int hotorder=0;
+            int company_id=0;
+
+
+            bool isok = new DAO_project().Add(name_en, name,  description,  img_url,  poi_id,  iv_id,  listorder,  ishot,  hotorder,  company_id,  industry_id);
+
+            if (isok == true)
+            {
+                var send = new
+                {
+                    msg = "操作成功！",
+                    success = "true"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+
+        }
+
+
+        public string Update(int id, string name_en,string name, string description, string iv_id,  bool ishot, int industry_id)
+        {
+            string img_url = "image/project/" + iv_id + "/title.jpg";
+            string poi_id = "0";
+            int listorder = 0;
+            int hotorder = 0;
+            int company_id = 0;
+
+
+            bool isok = new DAO_project().Update(id,name_en, name, description, img_url, poi_id, iv_id, listorder, ishot, hotorder, company_id, industry_id);
+
+            if (isok == true)
+            {
+                var send = new
+                {
+                    msg = "操作成功！",
+                    success = "true"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+            else
+            {
+                var send = new
+                {
+                    msg = "操作失败！",
+                    success = "false"
+                };
+                return JsonConvert.SerializeObject(send);
+            }
+
         }
     } 
 }
